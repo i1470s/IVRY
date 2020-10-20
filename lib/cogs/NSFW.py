16f4847e9fe54c53142ab1eca9643c68c94f5512
brayden viana <55937162+i1470s@users.noreply.github.com>
2020-10-19 20:23:36 -0700

    Getting ready for V.1.4

--- lib/cogs/NSFW.py
+++ lib/cogs/NSFW.py
@@ -3,11 +3,11 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix='.')
 
-class Games(commands.Cog):   
+class NSFW(commands.Cog):   
     
         def __init__(self, client):
                 self.client = client
 
 
 def setup(client):
-    client.add_cog(Games(client))
+    client.add_cog(NSFW(client))
