c6e446125930fd0787de2c97eccfcd9d24a690aa
Brayden Viana <55937162+i1470s@users.noreply.github.com>
2020-12-11 17:38:48 -0800

    Delete update.sh

--- update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -s https://api.github.com/repos/i1470s/IVRY-Discord-Bot/releases/latest \
-  | grep browser_download_url \
-  | grep linux64 \
-  | cut -d '"' -f 4 \
-  | wget -qi -
