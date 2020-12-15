ff13b8d33e987bcc2ad06ed28298ae40f2939db5
Brayden Viana <55937162+i1470s@users.noreply.github.com>
2020-12-14 23:22:22 -0800

    MINOR REWRITE/RESTRUCTURE

---
+++ cogs/fun.py
@@ -0,0 +1,220 @@
+import random
+import math
+from random import choice, randint
+from typing import Optional
+import discord
+from discord.ext import commands
+from discord.ext.commands import cooldown
+import aiohttp
+from aiohttp import request
+import pyfiglet
+import datetime
+now = datetime.datetime.now()
+diff = datetime.datetime(now.year, 12, 25) - \
+    datetime.datetime.today() 
+diff2 = datetime.datetime(now.year, 10, 31) - \
+    datetime.datetime.today() 
+diff3 = datetime.datetime(now.year, 2, 14) - \
+    datetime.datetime.today() 
+
+class Fun(commands.Cog):   
+    
+        def __init__(self, client):
+                self.client = client   
+
+        @commands.command(name='8Ball', description="Awnsers your questions")
+        async def _8ball(self, ctx, *, question):
+                responses = ['It is certain',
+                        'It is decidedly so',
+                        'Without a doubt',
+                        'Yes Definitely',
+                        'You may rely on it',
+                        'Most likely',
+                        'Yes',
+                        'No',
+                        'Dont count on it',
+                        'Can not predict now',
+                        'the reply is no',
+                        'very doubtful']
+                await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+        
+        @commands.command(name='joke', description="Tells you a joke")
+        async def joke(self, ctx):
+
+                colour_choices= [0x9B59B6]
+
+                meme_url = "https://sv443.net/jokeapi/v2/joke/Miscellaneous,Pun,Spooky,Christmas?type=single"
+                async with request("GET", meme_url, headers={}) as response:
+                        if response.status==200:
+                                data = await response.json()
+                                joke = data["joke"]
+
+                                await ctx.send(f"{joke}")
+
+                        else:
+                                await ctx.send(f"The API seems down, says {response.status}")
+
+        @commands.command(name='djoke', description="Dark jokes")
+        async def djoke(self, ctx):
+
+                colour_choices= [0x9B59B6]
+
+                meme_url = "https://sv443.net/jokeapi/v2/joke/Dark?type=single"
+                async with request("GET", meme_url, headers={}) as response:
+                        if response.status==200:
+                                data = await response.json()
+                                joke = data["joke"]
+
+                                await ctx.send(f"{joke}")
+
+                        else:
+                                await ctx.send(f"The API seems down, says {response.status}")
+
+        @commands.command(name='meme', description="Sends a meme")
+        async def meme(self, ctx):
+
+                colour_choices= [0x9B59B6]
+
+                meme_url = "https://meme-api.herokuapp.com/gimme?"
+                async with request("GET", meme_url, headers={}) as response:
+                        if response.status==200:
+                                data = await response.json()
+                                image_link = data["url"]
+                        else:
+                                image_link = None
+
+                        async with request("GET", meme_url, headers={}) as response:
+                                if response.status==200:
+                                        data = await response.json()
+                                        embed = discord.Embed(
+                                        title=data["title"],
+                                        colour=random.choice(colour_choices),
+                                        )
+                                        if image_link is not None:
+                                                embed.set_image(url=image_link)
+                                                await ctx.send(embed=embed)
+
+                                        else:
+                                                await ctx.send(f"The API seems down, says {response.status}")
+
+        @commands.command(name='measure', description="Measures your cock")
+        async def measure(self, ctx):
+                responses = ['1 inches',
+                        '2 inches',
+                        '3 inches',
+                        '4 inches',
+                        '5 inches',
+                        '6 inches',
+                        '7 inches',
+                        '8 inches',
+                        '9 inches',
+                        '10 inches',
+                        '11 inches',
+                        '12 inches',]
+                await ctx.send(f'Whoa You Got {random.choice(responses)}')
+
+        @commands.command(name='fortune', description="Checks your fortune")
+        async def fortune(self, ctx):
+                responses = ['$1 Dollar',
+                        '$2000 Dollars',
+                        '$0 Dollars',
+                        '$100000 Dollars',
+                        '$6 Dollars',
+                        '$18.95 Dollars',
+                        '$5 Dollars',
+                        '$900 Dollars',
+                        '$21 Dollars',
+                        '$420 Dollars',
+                        '$69 Dollars',
+                        '$1000000 Dollars',]
+                await ctx.send(f'You Got {random.choice(responses)}')
+
+        @commands.command(name='roll', description="Rolls a dice")
+        async def roll(self, ctx):
+                responses = ['1',
+                        '2',
+                        '3',
+                        '4',
+                        '5',
+                        '6',
+                        '7',
+                        '8',
+                        '9',
+                        '10',
+                        '11',
+                        '12',]
+                await ctx.send(f'You Got {random.choice(responses)}')
+
+        @commands.command(name='useless', description="Links useless websites")
+        async def useless(self, ctx):
+                responses = ['http://chihuahuaspin.com/',
+                        'http://www.staggeringbeauty.com/',
+                        'https://leekspin.com/',
+                        'http://whitetrash.nl/',
+                        'http://yeahlemons.com/',
+                        'http://dogs.are.the.most.moe/',
+                        'http://www.ouaismaisbon.ch/',
+                        'http://www.muchbetterthanthis.com/',
+                        'https://crouton.net/',
+                        'https://semanticresponsiveillustration.com/',
+                        'http://www.partridgegetslucky.com/',
+                        'http://www.trashloop.com/',
+                        'http://willthefuturebeaweso.me/',
+                        'https://r33b.net/',
+                        'http://spaceis.cool/',
+                        'http://www.fallingfalling.com/',
+                        'http://corndogoncorndog.com/',
+                        'http://ww12.infinitething.com/',
+                        'https://chrismckenzie.com/',
+                        'http://onemillionlols.com/',
+                        'https://iamawesome.com/',]
+                try:
+                        embed = discord.Embed(
+                                colour = 0x9B59B6
+                        )
+                        embed.add_field(name='Here is your useless website:', value=f'{random.choice(responses)}', inline=False)
+
+                        await ctx.send(embed=embed)
+                except Exception as e:
+                                await ctx.send(f'{e}')
+
+        @commands.command(name='hi', description="Hello :)")
+        async def hi(self, ctx):
+                await ctx.send(f'Hello :)')
+
+        @commands.command(name='fucku', description="Try it :p")
+        async def fucku(self, ctx):
+                await ctx.send(f'No Fuck You')
+        
+        @commands.command(name='xmas', description='Xmas countdown')
+        async def xmas(self, ctx):
+                await ctx.send("**{0}** day(s) left until Christmas day! :christmas_tree:".format(str(diff.days)))
+
+        @commands.command(name='hallow', description='Halloween countdown')
+        async def boo(self, ctx):
+                await ctx.send("**{0}** day(s) left until Halloween! :candy:".format(str(diff2.days)))
+        
+        @commands.command(name='vday', description='Valentines countdown')
+        async def vday(self, ctx):
+                await ctx.send("**{0}** day(s) left until Valentines day! :heart:".format(str(diff3.days)))
+
+        @commands.command(name='girlfriend', description="Take your shot")
+        async def girlfriend(self, ctx):
+                await ctx.send(f'Lol your dreaming you disrespecful trash!!!')
+
+        @commands.command(name='boyfriend', description="Take your shot")
+        async def boyfriend(self, ctx):
+                await ctx.send(f'Lol you think someone would want you????')
+
+        @commands.command(name='gay', description="Take the test")
+        async def gay(self, ctx, member: discord.Member = None):        
+                await ctx.send(f'Lemme check... ohhh its u lol, dumbass')
+
+        @commands.command(name='repeat', description="Repeats you")
+        async def repeat(self, ctx, *, message):
+                await ctx.send(message)  
+                
+                return
+
+def setup(client):
+    client.add_cog(Fun(client))
---
+++ cogs/help.py
@@ -0,0 +1,80 @@
+import random
+import discord
+import math
+from discord.ext import commands
+from data import config
+
+class Help(commands.Cog):   
+    
+        def __init__(self, client):
+                self.client = client        
+        
+
+        @commands.group(invoke_without_command=True)
+        async def help(self, ctx):
+            embed = discord.Embed(title = f"Prefix {config.prefix}", description = f"Use {config.default_prefix}help <catagory> for a list of commands", colour = 0x9B59B6)
+            embed.set_thumbnail(url=ctx.bot.user.avatar_url)
+            embed.set_footer(text=f"{config.version} | {config.shards}")
+            embed.set_author(name="IVRY Help", icon_url=ctx.bot.user.avatar_url)
+
+            embed.add_field(name = "General :gear:", value = "`8` General Commands", inline=False)
+            embed.add_field(name = "Fun :ribbon:", value="`17` Fun Commands", inline=False)
+            embed.add_field(name = "NSFW :x:", value="`9` NSFW Commands", inline=False)
+            embed.add_field(name = "Music :musical_note:", value="`10` Music Player Commands", inline=False)
+            embed.add_field(name = "Admin :hammer:", value="`6` Admin Commands", inline=False)
+            embed.add_field(name = "Additional Resources", value=":video_game: [IVRY Server](https://discord.gg/ppn2u99)\n:computer: [Website](https://ivry.tk)", inline=False)
+
+            await ctx.send(embed = embed)
+
+        @help.command()
+        async def general(self, ctx):
+            embed = discord.Embed(title = "IVRY Help - General :gear:", description = f"Prefixes {config.prefix}", colour = 0x9B59B6)
+            embed.set_thumbnail(url=ctx.bot.user.avatar_url)
+            embed.set_footer(text=f"{config.version} | {config.shards}")
+
+            embed.add_field(name = "General :gear:", value = "General Commands List \n\n `about`- Bot info \n `credits`- Bot credits \n `donate`- Bot donations \n `invite`- Bot invite link \n `ping`- Bot ping \n `servers`- Bot server list \n `serverinfo`- Current serverinfo \n `userinfo`- Get user info", inline=False)
+
+            await ctx.send(embed = embed)
+
+        @help.command()
+        async def fun(self, ctx):
+            embed = discord.Embed(title = "IVRY Help - Fun :ribbon:", description = f"Prefixes {config.prefix}", colour = 0x9B59B6)
+            embed.set_thumbnail(url=ctx.bot.user.avatar_url)
+            embed.set_footer(text=f"{config.version} | {config.shards}")
+
+            embed.add_field(name = "Fun :ribbon:", value = "Fun Commands List \n\n `8ball`- Guesses your fortune \n `joke`- Sends a joke \n `djoke`- Sends a dark joke \n `meme`- Sends a meme \n `measure`- Measures your cock \n `fortune`- Guesses your fortune \n `roll`- Roles a random number \n `useless`- Sends a useless website \n `hi`- Hey \n `fucku`- Try it \n `xmas`- Christmas countdown \n `hallow`- Halloween countdown \n `vday`- Valintines day countdown \n `girlfriend`- Shoot your shot \n `boyfriend`- Shoot your shot \n `gay`- Try it \n `repeat`- Repeats what you say", inline=False)
+
+            await ctx.send(embed = embed)
+
+        @help.command()
+        async def nsfw(self, ctx):
+            embed = discord.Embed(title = "IVRY Help - NSFW :x:", description = f"Prefixes {config.prefix}", colour = 0x9B59B6)
+            embed.set_thumbnail(url=ctx.bot.user.avatar_url)
+            embed.set_footer(text=f"{config.version} | {config.shards}")
+
+            embed.add_field(name = "NSFW :x:", value = "NSFW Commands List \n\n `ass`- Sends some ass \n `boobs`- Sends some boobs \n `red`- Sends some gingers \n `hentai`- Sends some hentai \n `lewd`- Sends some lewd \n `lesbians`- Sends some lesbians \n `teen`- Sends some teen pussy \n `random`- Sends some random NSFW \n `pussy`- Sends some pussy", inline=False)
+
+            await ctx.send(embed = embed)
+
+        @help.command()
+        async def music(self, ctx):
+            embed = discord.Embed(title = "IVRY Help - Music :musical_note:", description = f"Prefixes {config.prefix}", colour = 0x9B59B6)
+            embed.set_thumbnail(url=ctx.bot.user.avatar_url)
+            embed.set_footer(text=f"{config.version} | {config.shards}")
+
+            embed.add_field(name = "Music :musical_note:", value = "Music Commands List \n\n `join`- Join VC \n `leave`- Leave VC \n `play`- Play a song \n `skip`- Skip a song \n `queue`- Current song queue \n `shuffle`- Shuffle queue \n `remove`- Remove from queue \n `loop`- Loop song /n `now`- Currently playing /n `lyrics`- Search lyrics", inline=False)
+
+            await ctx.send(embed = embed)
+
+        @help.command()
+        async def admin(self, ctx):
+            embed = discord.Embed(title = "IVRY Help - Admin :hammer:", description = f"Prefixes {config.prefix}", colour = 0x9B59B6)
+            embed.set_thumbnail(url=ctx.bot.user.avatar_url)
+            embed.set_footer(text=f"{config.version} | {config.shards}")
+            
+            embed.add_field(name = "Admin :hammer:", value = "Admin Commands List \n\n `ban`- Ban members \n `kick`- Kick members \n `mute`- Mute members \n `unban`- Unban members \n `unmute`- Unmute members \n `clear`- Clear messages", inline=False)
+
+            await ctx.send(embed = embed)
+
+def setup(client):
+    client.add_cog(Help(client))
