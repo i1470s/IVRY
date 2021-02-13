35c3ba6d588f6b425cb7cb9e80edfb1f54b140d5
Brayden Viana <55937162+i1470s@users.noreply.github.com>
2021-02-13 15:43:49 -0800

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,13 +1,6 @@
 # /bin/sh
 while true
 do
-echo  _____  ____   ____  _______   ____  ____   
-echo |_   _||_  _| |_  _||_   __ \ |_  _||_  _|  
-echo   | |    \ \   / /    | |__) |  \ \  / /    
-echo   | |     \ \ / /     |  __ /    \ \/ /     
-echo  _| |_     \   /     _| |  \ \_  _|  |_     
-echo |_____|     \_/     |____| |___||______|
-echo    Created by i1470s#0396 Welcome :)
 echo
 echo ---PYTHON VERSION---
 echo
