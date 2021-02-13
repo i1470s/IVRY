73ecb5fc71c6a9d0c7d65397b9b76f845d728362
Brayden Viana <55937162+i1470s@users.noreply.github.com>
2021-02-13 15:23:48 -0800

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,12 +1,20 @@
 # /bin/sh
 while true
 do
-rm -rf /home/brayden/desktop/IVRYBot/IVRY-Discord-Bot
-git clone git@github.com:i1470s/IVRY-Discord-Bot.git 
-echo 
+echo ---PYTHON VERSION---
+echo
 python3 -c 'import sys; print(sys.version_info[:])'
 echo
+echo ---REPO UPDATER---
+echo
+rm -rf /home/brayden/desktop/IVRYBot/IVRY-Discord-Bot
+git clone git@github.com:i1470s/IVRY-Discord-Bot.git 
+echo
+echo ---PACKAGE UPDATER---
+echo
 python3 /home/brayden/desktop/IVRYBot/extras/package-updater.py
+echo
+echo ---BOT INIT---
 echo
 echo Starting Bot
 python3 bot.py
