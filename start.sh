d8a9266af3c2055530fae52296282040f340179d
Brayden Viana <55937162+i1470s@users.noreply.github.com>
2021-02-13 17:24:22 -0800

    Add files via upload

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 while true
 do
 echo
-echo ---IVRY INIT---
+echo ---IVRY INIT - Version - 1.0---
 echo
 sleep 3
 echo Created by i1470s#3096
@@ -15,13 +15,13 @@ echo
 echo ---REPO UPDATER---
 echo
 sleep 5
-rm -rf /home/brayden/desktop/IVRYBot/IVRY-Discord-Bot
+rm -rf /home/brayden/Desktop/IVRYBot/IVRY-Discord-Bot
 git clone git@github.com:i1470s/IVRY-Discord-Bot.git 
 echo
 echo ---PACKAGE UPDATER---
 echo
 sleep 5
-python3 /home/brayden/desktop/IVRYBot/extras/package-updater.py
+pip3 install -r requirements.txt --upgrade
 echo
 echo ---BOT INIT---
 echo
